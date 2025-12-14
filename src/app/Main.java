87c6577e1107455ad094275e2fb6ee83b53fbec4
Valentina Mussio Blanco <tina.mussio@icloud.com>
2025-12-15 12:07:04 +1300

    Initial Commit

---
+++ src/app/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package app;
+
+/**
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("BIT504_A2_Task9\n");
+		System.out.print("Hello World");
+
+	}
+
+}
+
